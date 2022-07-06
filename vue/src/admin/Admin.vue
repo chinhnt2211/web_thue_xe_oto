8f4704b38573c24f7516960ef6cfeacc09b106da
julyyv <chanvucktv@gmail.com>
2022-07-06 08:20:19 +0700

    update(vue): auth admin

--- vue/src/admin/Admin.vue
+++ vue/src/admin/Admin.vue
@@ -1,23 +1,34 @@
 <template>
-    <div id="admin">
-        <h1>Admin</h1>
-        
-        <AppHeader/>
-        <!-- {{> ThemeOptions/theme-options }} -->
-        <router-view></router-view>
+  <div id="admin">
+    <sidebar />
+    <div class="relative md:ml-64 bg-blueGray-100">
+      <admin-navbar />
+      <!-- <header-stats /> -->
+      <div class="px-4 md:px-10 mx-auto w-full pt-24">
+        <router-view />
+        <footer-admin />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import AppHeader from './layout/AppHeader/header.vue';
-
-export default {    
-    components: {
-        AppHeader
-    } 
-}
+import AdminNavbar from "@/admin/components/Navbars/AdminNavbar.vue";
+import Sidebar from "@/admin/components/Sidebar/Sidebar.vue";
+import HeaderStats from "@/admin/components/Headers/HeaderStats.vue";
+import FooterAdmin from "@/admin/components/Footers/FooterAdmin.vue";
+export default {
+  name: "admin-layout",
+  components: {
+    AdminNavbar,
+    Sidebar,
+    HeaderStats,
+    FooterAdmin,
+  },
+};
 </script>
 
-<style lang="scss">
-@import './assets/base.scss';
+<style>
+@import "@fortawesome/fontawesome-free/css/all.min.css";
+@import "@/admin/assets/styles/tailwind.css";
 </style>
--- vue/src/layouts/user/Home.vue
+++ vue/src/layouts/user/Home.vue
@@ -1,4 +1,6 @@
 <template >
+<h1>HOME</h1>
+<router-link to="{ name: 'Admin'}">Admin</router-link>
 </template>
 
 <script>
