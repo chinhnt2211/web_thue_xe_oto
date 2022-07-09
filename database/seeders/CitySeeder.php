4b95333806852af5562e373651d1a1b00106dbaa
julyyv <chanvucktv@gmail.com>
2022-07-09 19:54:35 +0700

    update(database): add city, district, subdistrict

---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\City;
+use Illuminate\Database\Seeder;
+
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+            City::factory()->create(['id' => '01', 'name' => 'Hà Nội']);
+            City::factory()->create(['id' => '26', 'name' => 'Vĩnh Phúc']);
+            City::factory()->create(['id' => '27', 'name' => 'Bắc Ninh']);
+            City::factory()->create(['id' => '30', 'name' => 'Hải Dương']);
+            City::factory()->create(['id' => '31', 'name' => 'Hải Phòng']);
+            City::factory()->create(['id' => '33', 'name' => 'Hưng Yên']);
+            City::factory()->create(['id' => '34', 'name' => 'Thái Bình']);
+            City::factory()->create(['id' => '35', 'name' => 'Hà Nam']);
+            City::factory()->create(['id' => '96', 'name' => 'Cà Mau']);
+            City::factory()->create(['id' => '02', 'name' => 'Hà Giang']);
+            City::factory()->create(['id' => '04', 'name' => 'Cao Bằng']);
+            City::factory()->create(['id' => '06', 'name' => 'Bắc Kạn']);
+            City::factory()->create(['id' => '08', 'name' => 'Tuyên Quang']);
+            City::factory()->create(['id' => '19', 'name' => 'Thái Nguyên']);
+            City::factory()->create(['id' => '20', 'name' => 'Lạng Sơn']);
+            City::factory()->create(['id' => '22', 'name' => 'Quảng Ninh']);
+            City::factory()->create(['id' => '24', 'name' => 'Bắc Giang']);
+            City::factory()->create(['id' => '25', 'name' => 'Phú Thọ']);
+            City::factory()->create(['id' => '10', 'name' => 'Lào Cai']);
+            City::factory()->create(['id' => '11', 'name' => 'Điện Biên']);
+            City::factory()->create(['id' => '12', 'name' => 'Lai Châu']);
+            City::factory()->create(['id' => '14', 'name' => 'Sơn La']);
+            City::factory()->create(['id' => '15', 'name' => 'Yên Bái']);
+            City::factory()->create(['id' => '17', 'name' => 'Hoà Bình']);
+            City::factory()->create(['id' => '70', 'name' => 'Bình Phước']);
+            City::factory()->create(['id' => '72', 'name' => 'Tây Ninh']);
+            City::factory()->create(['id' => '74', 'name' => 'Bình Dương']);
+            City::factory()->create(['id' => '75', 'name' => 'Đồng Nai']);
+            City::factory()->create(['id' => '79', 'name' => 'Hồ Chí Minh']);
+            City::factory()->create(['id' => '77', 'name' => 'Bà Rịa Vũng Tàu']);
+            City::factory()->create(['id' => '36', 'name' => 'Nam Định']);
+            City::factory()->create(['id' => '37', 'name' => 'Ninh Bình']);
+            City::factory()->create(['id' => '38', 'name' => 'Thanh Hóa']);
+            City::factory()->create(['id' => '40', 'name' => 'Nghệ An']);
+            City::factory()->create(['id' => '42', 'name' => 'Hà Tĩnh']);
+            City::factory()->create(['id' => '44', 'name' => 'Quảng Bình']);
+            City::factory()->create(['id' => '45', 'name' => 'Quảng Trị']);
+            City::factory()->create(['id' => '46', 'name' => 'Thừa Thiên Huế']);
+            City::factory()->create(['id' => '48', 'name' => 'Đà Nẵng']);
+            City::factory()->create(['id' => '49', 'name' => 'Quảng Nam']);
+            City::factory()->create(['id' => '51', 'name' => 'Quảng Ngãi']);
+            City::factory()->create(['id' => '52', 'name' => 'Bình Định']);
+            City::factory()->create(['id' => '54', 'name' => 'Phú Yên']);
+            City::factory()->create(['id' => '56', 'name' => 'Khánh Hòa']);
+            City::factory()->create(['id' => '58', 'name' => 'Ninh Thuận']);
+            City::factory()->create(['id' => '60', 'name' => 'Bình Thuận']);
+            City::factory()->create(['id' => '62', 'name' => 'Kon Tum']);
+            City::factory()->create(['id' => '64', 'name' => 'Gia Lai']);
+            City::factory()->create(['id' => '66', 'name' => 'Đắk Lắk']);
+            City::factory()->create(['id' => '67', 'name' => 'Đắk Nông']);
+            City::factory()->create(['id' => '68', 'name' => 'Lâm Đồng']);
+            City::factory()->create(['id' => '80', 'name' => 'Long An']);
+            City::factory()->create(['id' => '82', 'name' => 'Tiền Giang']);
+            City::factory()->create(['id' => '83', 'name' => 'Bến Tre']);
+            City::factory()->create(['id' => '84', 'name' => 'Trà Vinh']);
+            City::factory()->create(['id' => '86', 'name' => 'Vĩnh Long']);
+            City::factory()->create(['id' => '87', 'name' => 'Đồng Tháp']);
+            City::factory()->create(['id' => '89', 'name' => 'An Giang']);
+            City::factory()->create(['id' => '91', 'name' => 'Kiên Giang']);
+            City::factory()->create(['id' => '92', 'name' => 'Cần Thơ']);
+            City::factory()->create(['id' => '93', 'name' => 'Hậu Giang']);
+            City::factory()->create(['id' => '94', 'name' => 'Sóc Trăng']);
+            City::factory()->create(['id' => '95', 'name' => 'Bạc Liêu']);
+    }
+}
